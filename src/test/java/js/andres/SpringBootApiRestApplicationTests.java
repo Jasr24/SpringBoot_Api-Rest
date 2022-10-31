0718c35bc3554b808b51155f1093e74bb610a242
Andres <josex.1995@hotmail.com>
2022-10-31 12:10:50 -0500

    Primer commit- Creacion del projecto

---
+++ src/test/java/js/andres/SpringBootApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package js.andres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
